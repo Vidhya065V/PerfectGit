cb0bf8d081c4b01e0e5d59436825445dc1543d23
vidhya065v <vidhya065vidarba@gmail.com>
2022-03-16 13:17:33 +0530

    Project code push

---
+++ src/orggit/GitHub.java
@@ -0,0 +1,25 @@
+package orggit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.interactions.Actions;
+
+public class GitHub {
+	public static void main(String[] args) {
+		
+	
+	int a=10;
+	int b=20;
+	int c=a+b;
+	
+	System.out.println(c);
+	
+		
+		
+		
+	}
+
+}
